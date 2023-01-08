ad68d25d8ec6559caedf25b5aeb2601c3311ae84
root <root@DESKTOP-EPQT6OB.localdomain>
2023-01-08 12:42:18 -0300

    feat: create dto to create a product

---
+++ src/usecase/product/create/create.product.dto.ts
@@ -0,0 +1,12 @@
+
+export interface InputCreateProductDto {
+    name: string;
+    price: string;
+}
+
+
+export interface OutputCreateProductDto {
+    id: string;
+    name: string;
+    price: string;
+}
