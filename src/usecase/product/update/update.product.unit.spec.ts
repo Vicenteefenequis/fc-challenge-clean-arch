cc16296415f711255b57889bff8af6d94907dba7
root <root@DESKTOP-EPQT6OB.localdomain>
2023-01-08 13:55:12 -0300

    feat: ensure UpdateProductUseCase update a product

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,39 @@
+import ProductFactory from "../../../domain/product/factory/product.factory"
+import UpdateProductUseCase from "./update.product.usecase";
+
+const product = ProductFactory.create("a", "Tenis", 25.99)
+
+
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        update: jest.fn(),
+        findAll: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product))
+    }
+}
+
+
+
+
+describe('Unit test update product usecase', () => {
+    test('should updated a product', async () => {
+
+        const productMockRepository = MockRepository()
+        const usecase = new UpdateProductUseCase(productMockRepository)
+
+        const input = {
+            id: product.id,
+            name: "Tenis updated",
+            price: 30.99
+        }
+
+
+        const output = await usecase.execute(input)
+
+
+        expect(output).toEqual({ id: input.id, name: input.name, price: input.price })
+
+    });
+});
