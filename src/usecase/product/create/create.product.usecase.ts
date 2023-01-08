4d90e27e0d4444202af6ee698dfae5b59b0c891a
root <root@DESKTOP-EPQT6OB.localdomain>
2023-01-08 13:45:53 -0300

    refactor: change to use factory off product

--- src/usecase/product/create/create.product.usecase.ts
+++ src/usecase/product/create/create.product.usecase.ts
@@ -1,7 +1,7 @@
 import Product from "../../../domain/product/entity/product";
+import ProductFactory from "../../../domain/product/factory/product.factory";
 import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
 import { InputCreateProductDto, OutputCreateProductDto } from "./create.product.dto";
-import { v4 as uuid } from 'uuid'
 
 export default class CreateProductUseCase {
     constructor(private readonly productRepository: ProductRepositoryInterface) { }
@@ -10,7 +10,8 @@ export default class CreateProductUseCase {
 
 
     async execute(input: InputCreateProductDto): Promise<OutputCreateProductDto> {
-        const product = new Product(uuid(), input.name, input.price)
+        const productFactory = ProductFactory.create("a", input.name, input.price)
+        const product = new Product(productFactory.id, productFactory.name, productFactory.price)
         await this.productRepository.create(product)
 
         return {
