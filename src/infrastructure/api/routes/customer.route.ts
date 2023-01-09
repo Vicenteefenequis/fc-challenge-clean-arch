4230baa98b5f88975c875c171260737928d50ac6
Vicente Junior <vicente19981@live.com>
2023-01-09 12:13:02 -0300

    feat: create e2e customer spec

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -1,36 +1,40 @@
-import express, { Request, Response } from "express";
-import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
-import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
-import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
-import CustomerPresenter from "../presenters/customer.presenter";
+import express, { request, Request, Response } from 'express'
+import CreateCustomerUseCase from '../../../usecase/customer/create/create.customer.usecase'
+import CustomerRepository from '../../../infrastructure/customer/repository/sequelize/customer.repository'
+import ListCustomerUseCase from '../../../usecase/customer/list/list.customer.usecase'
+export const customerRoute = express.Router()
 
-export const customerRoute = express.Router();
+customerRoute.post('/', async (req: Request, res: Response) => {
+    const usecase = new CreateCustomerUseCase(new CustomerRepository())
 
-customerRoute.post("/", async (req: Request, res: Response) => {
-  const usecase = new CreateCustomerUseCase(new CustomerRepository());
-  try {
-    const customerDto = {
-      name: req.body.name,
-      address: {
-        street: req.body.address.street,
-        city: req.body.address.city,
-        number: req.body.address.number,
-        zip: req.body.address.zip,
-      },
-    };
-    const output = await usecase.execute(customerDto);
-    res.send(output);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+    try {
+        const customerDto = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                number: req.body.address.number,
+                zip: req.body.address.zip,
+                city: req.body.address.city
+            }
+        }
 
-customerRoute.get("/", async (req: Request, res: Response) => {
-  const usecase = new ListCustomerUseCase(new CustomerRepository());
-  const output = await usecase.execute({});
+        const output = await usecase.execute(customerDto)
+        res.send(output)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
 
-  res.format({
-    json: async () => res.send(output),
-    xml: async () => res.send(CustomerPresenter.listXML(output)),
-  });
-});
+customerRoute.get('/', async (req: Request, res: Response) => {
+    const usecase = new ListCustomerUseCase(new CustomerRepository())
+
+    try {
+        const customerDto = {}
+
+        const output = await usecase.execute(customerDto)
+        res.send(output)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+
+})
